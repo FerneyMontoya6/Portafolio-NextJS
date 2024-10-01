6aadb30d6abf4177fe899257dac42f93a6e21d73
Ferney <ferney.montoyag@gmail.com>
2024-10-01 09:26:00 -0500

    Deploy set up

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// next.config.js
+module.exports = {
+  output: 'export',
+  basePath: '/<despliegue-next>',
+  trailingSlash: true,
+}
+
+
 export default nextConfig;
