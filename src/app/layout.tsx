cc17c082560369ca18e649fda76bfe6822ff02d0
Ferney <ferney.montoyag@gmail.com>
2024-10-01 09:32:44 -0500

    Default font variables deleted

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Inter } from 'next/font/google'
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const inter = Inter({
   subsets: ['latin'],
